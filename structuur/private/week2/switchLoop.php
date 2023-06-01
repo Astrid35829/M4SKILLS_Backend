e1dcf0186a28fa100d2c85dd17e151fa85b096dc
Astrid <35829@ma-web.nl>
2023-06-01 10:01:06 +0200

    added week 1 and 2

---
+++ structuur/private/week2/switchLoop.php
@@ -0,0 +1,91 @@
+<?php
+// php.net date function
+$number_month = date(format: 'm');
+switch ($number_month){
+    case 1:
+        echo "January";
+        break;
+    case 2:
+        echo "February";
+        break;
+    case 3:
+        echo "March";
+        break;
+    case 4:
+        echo "April";
+        break;
+    case 5:
+        echo "May";
+        break;
+    case 6:
+        echo "June";
+        break;
+    case 7:
+        echo "July";
+        break;
+    case 8:
+        echo "Augustus";
+        break;
+    case 9:
+        echo "September";
+        break;
+    case 10:
+        echo "October";
+        break;
+    case 11:
+        echo "November";
+        break;
+    case 12:
+        echo "December";
+    default:
+        echo "Did you know there are rumors about a 13th zodiac sign adding Ophiuchus the Snake Charmer between November 30th and December 18th.";              
+}
+print '</br>';
+
+$name_month = "Aug";
+switch ($name_month){
+    case "Jan":
+        echo "January";
+        break;
+    case "Feb":
+        echo "February";
+        break;
+    case "Mar":
+        echo "March";
+        break;
+    case "Apr":
+        echo "April";
+        break;
+    case "May":
+        echo "May";
+        break;
+    case "Jun":
+        echo "June";
+        break;
+    case "Jul":
+        echo "July";
+        break;
+    case "Aug":
+        echo "Augustus";
+        break;
+    case "Sep":
+        echo "September";
+        break;
+    case "Oct":
+        echo "October";
+        break;
+    case "Nov":
+        echo "November";
+        break;
+    case "Dec":
+        echo "December";
+    default:
+        echo "Did you know there are rumors about a 13th zodiac sign adding Ophiuchus the Snake Charmer between November 30th and December 18th.";              
+}
+print '</br>';
+
+
+
+
+
+?>
