60ae73f093359fcf97481d6b82039a4b758bd1c8
Astrid <35829@ma-web.nl>
2023-06-01 11:13:06 +0200

    added private week 3 opdr 1,2,3,4

---
+++ structuur/private/week3/week3.php
@@ -0,0 +1,11 @@
+<?php
+
+function cijfers($a = 2, $b = 4, $c = 6, $d = 8, $e = 10){
+    $numbers = [$a, $b, $c, $d, $e];
+    /* Extra: foreach argument loop through and put in array? */
+    /* foreach(i = 0; $numbers.length; i++){
+        print_r($numbers);
+       }*/
+    print_r($numbers);
+}
+
---
+++ structuur/private/week3/week3uitvoering.php
@@ -0,0 +1,6 @@
+<?php
+include('week3.php');
+
+cijfers();
+print '</br>';
+cijfers(1, 2, 3, 4, 5);
